df3a1062405d77a681f97bbb81bd8409755e9505
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 14:20:13 +0500

    heroku

--- server/index.js
+++ server/index.js
@@ -10,7 +10,14 @@ app.use(cors());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-    res.json({ message: 'Welcome to the server' });
+    res.sendFile(
+        path.join(__dirname, "../toa-spa/build/index.html"),
+        function (err) {
+            if (err) {
+                res.status(500).send(err);
+            }
+        }
+    )
 });
 
 app.use('/api', require('./routes/authRouter'));
