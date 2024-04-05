52c314ce3361187a2414cc72766d186f52208470
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 12:47:59 +0500

    asdasdasd

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
@@ -12,7 +13,21 @@ app.get("/", (req, res) => {
     res.json({ message: 'Welcome to the server' });
 });
 
-// Temporarily comment out the auth router usage
-// app.use('/api', require('./routes/authRouter'));
+app.use('/api', require('./routes/authRouter'));
+
+const URI = process.env.MONGODB_URL;
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => console.log("Server ready on port 3000."));
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process if unable to connect to MongoDB
+});
 
 module.exports = app;
+
