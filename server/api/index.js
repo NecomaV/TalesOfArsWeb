294bba9a649e710e2e738b8c7c44e2711f4a644f
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 11:55:24 +0500

    fix arch

--- server/api/index.js
+++ server/api/index.js
@@ -22,10 +22,7 @@ mongoose.connect(URI, {
 })
 .then(() => {
     console.log('Connected to MongoDB');
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-        console.log('Server is running on port', port);
-    });
+    
 })
 .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
