ea18f7e54e768837b12c1a097f0ae296e43b335a
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 15:36:06 +0500

    asdas

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -125,16 +125,16 @@ const authCtrl = {
 
 
 
-// const createAccessToken = (payload) => {
-//     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
-// }
+const createAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
 
-// const createRefreshToken = (payload) => {
-//     return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'})
-// }
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '30d'})
+}
 
 
-// module.exports = authCtrl
+module.exports = authCtrl
 
 
 
