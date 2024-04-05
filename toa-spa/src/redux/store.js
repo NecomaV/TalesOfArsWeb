d546f317b9f70315c7904d3a1983a2d089f23d3b
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 11:46:08 +0500

    thuk upadye

--- toa-spa/src/redux/store.js
+++ toa-spa/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 import { composeWithDevTools } from 'redux-devtools-extension'; 
 
