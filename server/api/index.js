327394e44e6c6da50431e67be4444fd12c32756c
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-05 12:43:33 +0500

    check withoput mongodb

--- server/api/index.js
+++ server/api/index.js
@@ -1,41 +1,17 @@
-// require('dotenv').config();
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-// app.use(cookieParser());
-
-// app.get("/", (req, res) => {
-//     res.json({ message: 'Welcome to the server' });
-// });
-
-// app.use('/api', require('./routes/authRouter'));
-
-// const URI = process.env.MONGODB_URL;
-// mongoose.connect(URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .then(() => {
-//     console.log('Connected to MongoDB');
-//     app.listen(3000, () => console.log("Server ready on port 3000."));
-// })
-// .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-//     process.exit(1); // Exit the process if unable to connect to MongoDB
-// });
-
-// module.exports = app;
-
-const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+    res.json({ message: 'Welcome to the server' });
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use('/api', require('./routes/authRouter'));
 
 module.exports = app;
