128d6b0f509633f53185b3c1fc5ebbb961734136
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-06 11:10:21 +0500

    path

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
