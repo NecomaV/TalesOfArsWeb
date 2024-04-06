223a05349a2163825879635a75d4d14074e37983
NecomaV <200103535@stu.sdu.edu.kz>
2024-04-06 11:19:46 +0500

    asd

--- server/index.js
+++ server/index.js
@@ -10,18 +10,13 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../toa-spa/build'))); 
+app.use(express.static(path.join(__dirname, 'app/toa-spa/build')));
 
-app.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../toa-spa/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
-    }
-  )
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/app/toa-spa/build/index.html'));
 });
+
+
 
 app.use('/api', require('./routes/authRouter'));
 
@@ -32,6 +27,12 @@ if (!URI) {
     process.exit(1);
 }
 
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
+
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -39,7 +40,6 @@ mongoose.connect(URI, {
   .then(() => {
     console.log('Connected to MongoDB');
 
-    app.listen(process.env.PORT || 3000, () => console.log("Server ready on port 3000."));
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
